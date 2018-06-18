139f757c1ee097cb1f90d026eb431817c6894b42
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-06-18 11:59:27 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot()
-  puts "Squawk!"
-  return
+def parrot(phrase = "Squawk!")
+  puts
+  return phrase
 end
+
+parrot
